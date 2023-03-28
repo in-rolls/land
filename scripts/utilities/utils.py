b8f789184e9120eeafbfa6f5f671e66cd9d1364e
LSYS <lucas@lucasshen.com>
2023-03-28 20:46:53 +0800

    Remove low_memory setting
    Pyarrow (Pandas 2.0) does not support low_memory option. I always have the option to set low_memory using the **pandas_kwargs if I need.

--- scripts/utilities/utils.py
+++ scripts/utilities/utils.py
@@ -8,7 +8,7 @@ def get_fulldata(directory="../data/bihar_land_records_csv/", **pandas_kwargs):
     final_df = []
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
-        lr_file = pd.read_csv(f, low_memory=False, **pandas_kwargs)
+        lr_file = pd.read_csv(f, **pandas_kwargs)
         final_df.append(lr_file)
 
     final_frame = pd.concat(final_df, axis=0, ignore_index=True)
